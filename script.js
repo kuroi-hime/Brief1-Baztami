c0953384fccd56060f5e3716491abf9a6ce7c085
kuroi-hime <kuroihime095@gmail.com>
2025-10-31 18:04:56 +0100

    feat: modification ajout transaction

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let button_nouvelle = document.getElementById("nouvelle");
 let button_ajouter = document.getElementById("ajouter");
 let button_annuler = document.getElementById("annuler");
+let myForm = document.getElementById('myForm');
 let count = localStorage.length;
 let total_revenus = 0;
 let total_depenses = 0;
@@ -8,10 +9,12 @@ let total_depenses = 0;
 function loadData(){
     for(let i=0; i<count; i++){
         let argent = JSON.parse(localStorage.getItem(i));
-        if(argent.type=="revenu")
-            total_revenus += Number(argent.montant);
-        else{
-            total_depenses += Number(argent.montant);
+        if(argent.status != 'deleted'){
+            if(argent.type=="revenu")
+                total_revenus += Number(argent.montant);
+            else{
+                total_depenses += Number(argent.montant);
+            }
         }
     }
     document.getElementById("revenus").innerText = total_revenus;
@@ -25,18 +28,34 @@ function ouvrir() {
 
 button_nouvelle.addEventListener("click", ouvrir);
 
+function validateForm() {
+
+  let montant = document.forms["myForm"]["montant"].value;
+  let date = document.forms["myForm"]["date"].value;
+  let description = document.forms["myForm"]["description"].value;
+
+  if (montant && date && description != "") {
+    button_ajouter.removeAttribute('disabled');
+  }
+}
+
+myForm.addEventListener('change', validateForm);
+
 function ajouterTransaction() {
-    localStorage.setItem(count++, JSON.stringify(
-        {montant: document.getElementById("montant").value,
+    localStorage.setItem(count, JSON.stringify(
+        {status: 'added',
+        id: count,
+        montant: document.getElementById("montant").value,
         type: document.getElementById("type").value,
         description: document.getElementById("description").value,
         date: document.getElementById("date").value}));
+    count++;
 }
 
 button_ajouter.addEventListener("click", ajouterTransaction);
 
 function fermer() {
-    document.getElementById("dialog").style.display = 'hidden';
+    document.getElementById("dialog").classList.add('hidden');
 }
 
 button_annuler.addEventListener("click", fermer);
